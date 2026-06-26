4f97fbbd9413ba7acf920f7a8a7f9e4b9fe2e6f4
VMIAO-bx <VMIAO-bx@users.noreply.github.com>
2026-06-26 08:11:04 +0000

    deploy: a1846a9a9c545464539e7c9905ec13579585c434

---
+++ js/chunk-C3p9YJJP.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-1oVRK5OI.js";import{Q as o,o as s,P as t}from"./chunk-Dzc2KEt2.js";const n=r({},[["render",function(r,n){const a=t("router-view");return s(),o(a)}]]);export{n as default};
